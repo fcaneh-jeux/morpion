710980699b78840c0cf7722ad832d4fc16ff82d0
fcanehan <fcanehan.jobs@gmail.com>
2026-03-10 10:37:46 +0100

    creation grille

--- scripts/Game.cs
+++ scripts/Game.cs
@@ -3,7 +3,20 @@ using System;
 
 public partial class Game : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
+	/// creation de la grille	
+	 // taille de la grille
+	const int SIZE = 3;
+
+	// 0 = vide, 1 = joueur1, 2 = joueur2
+	int[,] board = new int[SIZE, SIZE];
+
+	// joueur courant
+	int currentPlayer = 1;
+
+	// compteur de tours
+	int turn = 0;
+
+	
 	public override void _Ready()
 	{
 	}
